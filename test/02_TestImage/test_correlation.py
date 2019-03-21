8c369c61b83547314be6a692f6c89e219772f2a2
hensleyl4 <lauren.marieh7@gmail.com>
2019-03-21 10:34:37 -0400

    Plot the average of the correlation.

--- test/02_TestImage/test_correlation.py
+++ test/02_TestImage/test_correlation.py
@@ -38,32 +38,52 @@ if plot:
     plt.imshow(np.abs(actual-samples[0]), cmap='gray')
     plt.title('diff between actual')
     plt.draw()
-print(samples.shape)
-size = 200
-coeff = np.zeros(size)
-for k in range(1, size+1):
+
+def select_points(dist):
+    #todo; make pt1 selection avoid the while loop case
+    #i.e. only select from applicable points for pt1
     pt1 = (random.randint(0, samples.shape[1]-1), random.randint(0, samples.shape[2]-1))
     choices = circle(samples.shape[1:], pt1, k)
-    print(choices)
     while not choices: #make sure there were points within range for distance
         pt1 = (random.randint(0, samples.shape[1]-1), random.randint(0, samples.shape[2]-1))
-        choices = circle(samples.shape[1:], pt1, k)
+        choices = circle(samples.shape[1:], pt1, dist)
     pt2 = random.choice(choices)
-    if k % 50 == 0:
-        pts = np.asarray(circle(samples.shape[1:], pt1, k))
-        plt.figure()
-        plt.scatter(pts[:,0], pts[:,1])
-        plt.scatter(pt1[0], pt1[1], c='r')
-        plt.xlim(0,256)
-        plt.ylim(0,256)
-        plt.title('dist' + str(k))
-        plt.draw()
-    var1 = np.abs(samples[:,pt1[0], pt1[1]])
-    var2 = np.abs(samples[:,pt2[0], pt2[1]])
-    rho = np.corrcoef(np.asarray([var1, var2]))
-    coeff[k-1] = rho[0,1]
+    return pt1,pt2
+print(samples.shape)
+size = 200
+num_pts = 50
+coeff = np.zeros((size, num_pts))
+corravg = np.zeros(size)
+for k in range(1, size+1):
+    points = [None]*num_pts
+    for j in range(num_pts):
+        pt1, pt2 = select_points(k)
+        while (pt1,pt2) in points: #select new points if we've already looked at these
+            pt1, pt2 = select_points(k)
+        points[j] = (pt1,pt2)
+        if k % 50 == 0 and j == 1:
+            pts = np.asarray(circle(samples.shape[1:], pt1, k))
+            plt.figure()
+            plt.scatter(pts[:,0], pts[:,1])
+            plt.scatter(pt1[0], pt1[1], c='r')
+            plt.xlim(0,256)
+            plt.ylim(0,256)
+            plt.title('dist' + str(k))
+            plt.draw()
+        var1 = np.abs(samples[:,pt1[0], pt1[1]])
+        var2 = np.abs(samples[:,pt2[0], pt2[1]])
+        coeff[k-1, j] = np.corrcoef(np.asarray([var1, var2]))[0,1]
+    corravg[k-1] = np.mean(coeff[k-1])
 plt.figure()
-plt.plot(range(1,size+1), np.abs(coeff))
+plt.plot(range(1,size+1), np.abs(corravg))
 plt.xlabel('distance')
 plt.ylabel('abs of correlation coefficient')
+plt.title('Avg for ' + str(num_pts) + ' points at each distance')
+plt.draw()
+
+plt.figure()
+plt.boxplot(coeff.T)
+plt.xlabel('distance')
+plt.ylabel('abs of correlation coefficient')
+plt.title('boxplot')
 plt.show()
