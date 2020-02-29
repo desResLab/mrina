9d4b74d1c527e36950e305df6173f6c23cb15f7c
Daniele <daniele.schiavazzi@gmail.com>
2020-02-28 22:06:46 -0500

    added argparse to vtktoimg

--- vtktoimg.py
+++ vtktoimg.py
@@ -4,6 +4,7 @@ from vtk.util.numpy_support import numpy_to_vtk
 import os
 import sys
 import numpy as np
+import argparse
 home = os.getenv('HOME')
 
 def getInput(reader, filename):
@@ -30,21 +31,100 @@ def getInputData(directory, vtkfile):
     data = np.concatenate((concentration,velocity),axis=0)
     return data
 
-def saveInput(fromdir, vtkfile, todir, imagefile, sliceIndex, ext='.png', numpy=False):
-    inp = getInputData(directory, vtkfile)
-    print(inp.shape)
+def saveInput(fromdir,vtkfile,todir,imagefile,sliceIndex,printlevel):
+    inp = getInputData(fromdir, vtkfile)
+    if(printlevel>0):
+      print(inp.shape)
     inp = np.moveaxis(inp, 1+sliceIndex, 1)
     np.save(todir + imagefile + '.npy', inp)
     return inp
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        directory = sys.argv[1]
-        tosavedir = sys.argv[2]
-        sliceIndex = int(sys.argv[3])
-    else: 
-        directory = home + "/apps/pDat/samp256/"
-        tosavedir = home + '/apps/undersampled/poiseuille/img/'
-        sliceIndex = 2
 
-    saveInput(directory, 'pout0_0.vtk', tosavedir, 'imgs_n1', sliceIndex=sliceIndex)
+    parser = argparse.ArgumentParser(description='Convert VTK rectilinear grid to compressed image.')
+    
+    parser.add_argument('-o', '--origindir',
+                        action=None,
+                        # nargs='+',
+                        const=None,
+                        default='/apps/pDat/samp256/',
+                        type=str,
+                        choices=None,
+                        required=False,
+                        help='origin VTK file folder',
+                        metavar='',
+                        dest='directory')
+
+    parser.add_argument('-d', '--destdir',
+                        action=None,
+                        # nargs='+',
+                        const=None,
+                        default='/apps/undersampled/poiseuille/img/',
+                        type=str,
+                        choices=None,
+                        required=False,
+                        help='destination image folder',
+                        metavar='',
+                        dest='tosavedir')
+
+    parser.add_argument('-s', '--sliceindex',
+                        action=None,
+                        # nargs='+',
+                        const=None,
+                        default=2,
+                        type=str,
+                        choices=None,
+                        required=False,
+                        help='slice index',
+                        metavar='',
+                        dest='sliceindex')
+
+    parser.add_argument('-v', '--vtkname',
+                        action=None,
+                        # nargs='+',
+                        const=None,
+                        default='pout0_0.vtk',
+                        type=str,
+                        choices=None,
+                        required=False,
+                        help='name of the origin VTK file',
+                        metavar='',
+                        dest='vtkname')
+
+    parser.add_argument('-f', '--imagename',
+                        action=None,
+                        # nargs='+',
+                        const=None,
+                        default='imgs_n1.npy',
+                        type=str,
+                        choices=None,
+                        required=False,
+                        help='name of the destination image file',
+                        metavar='',
+                        dest='imagename')
+
+    parser.add_argument('-p', '--printlevel',
+                        action=None,
+                        # nargs='+',
+                        const=None,
+                        default=0,
+                        type=str,
+                        choices=None,
+                        required=False,
+                        help='print level, 0 - no print, >0 increasingly more information ',
+                        metavar='',
+                        dest='printlevel')
+
+    # Parse Commandline Arguments
+    args = parser.parse_args()
+
+    # Convert
+    saveInput(args.directory, 
+              args.vtkname, 
+              args.tosavedir, 
+              args.imagename, 
+              args.sliceindex,
+              args.printlevel)
+
+    if(args.printlevel > 0):
+      print('Completed!!!')
