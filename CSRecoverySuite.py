fixed consistent normalization of Fourier transform issue

CSRecoverySuite.py
@@ -20,7 +20,7 @@ def get_umask_string(samptype):
     sys.exit(-1)
 
 def extractFluidMask(img):  
-  res = np.max(np.absolute(img[0,0]),axis=0)
+  res = np.max(np.absolute(img),axis=0)[0,0]
   return (res > 0)    
 
 def get_method_string(method):
@@ -170,7 +170,7 @@ class Operator4dFlow(genericOperator):
     # Restrict the set of wavelet basis
     self.basisSet = basisSet
     # Constant
-    self._cst           = math.pow( np.prod( insz ), -1/2 );
+    # self._cst           = math.pow( np.prod( insz ), -1/2 );
 
   def eval(self, x, mode):
     '''
@@ -180,19 +180,26 @@ class Operator4dFlow(genericOperator):
     '''
     if( mode == 1 ): # FORWARD MAP
       if( self.samplingSet is None ):
-        return self._cst * fft.fft2(pywt.waverec2(array2pywt(x), wavelet=self.waveletName, mode=self.waveletMode));
+        # return self._cst * fft.fft2(pywt.waverec2(array2pywt(x), wavelet=self.waveletName, mode=self.waveletMode));
+        return fft.fft2(pywt.waverec2(array2pywt(x), wavelet=self.waveletName, mode=self.waveletMode),norm='ortho');
       else:
-        y = self._cst * fft.fft2(pywt.waverec2(array2pywt(x), wavelet=self.waveletName, mode=self.waveletMode));
+        # y = self._cst * fft.fft2(pywt.waverec2(array2pywt(x), wavelet=self.waveletName, mode=self.waveletMode));
+        y = fft.fft2(pywt.waverec2(array2pywt(x), wavelet=self.waveletName, mode=self.waveletMode),norm='ortho');
         return y[ self.samplingSet ];
     if( mode == 2 ): # ADJOINT MAP
       if( self.samplingSet is None ):
-        arr = np.conj( fft.fft2( np.conj(x) ) )
-        return self._cst * pywt2array(pywt.wavedec2(arr, wavelet=self.waveletName, mode=self.waveletMode), arr.shape);
+        # arr = np.conj( fft.fft2( np.conj(x) ) )
+        # return self._cst * pywt2array(pywt.wavedec2(arr, wavelet=self.waveletName, mode=self.waveletMode), arr.shape);
+        arr = np.conj( fft.fft2( np.conj(x),norm='ortho' ) )
+        return pywt2array(pywt.wavedec2(arr, wavelet=self.waveletName, mode=self.waveletMode), arr.shape);
       else:
         #y = np.zeros( self.imsz ) + 1j * np.zeros( self.imsz );
         self._buffer[ self.samplingSet ] = x[ : ];
-        arr = np.conj( fft.fft2( np.conj(self._buffer) ) )
-        return self._cst * pywt2array(pywt.wavedec2(arr, wavelet=self.waveletName, mode=self.waveletMode),arr.shape);
+        # arr = np.conj( fft.fft2( np.conj(self._buffer) ) )
+        # return self._cst * pywt2array(pywt.wavedec2(arr, wavelet=self.waveletName, mode=self.waveletMode),arr.shape);
+        arr = np.conj( fft.fft2( np.conj(self._buffer),norm='ortho' ) )
+        return pywt2array(pywt.wavedec2(arr, wavelet=self.waveletName, mode=self.waveletMode),arr.shape);
+
 
   #  The method returns the shape of the input array
   def input_size(self):
@@ -238,7 +245,8 @@ class Operator4dFlow(genericOperator):
       inV = inV.reshape(self.insz)
 
       # Compute the vector y
-      y = self._cst * fft.fft2(pywt.waverec2(array2pywt(inV), wavelet=self.waveletName, mode=self.waveletMode))
+      # y = self._cst * fft.fft2(pywt.waverec2(array2pywt(inV), wavelet=self.waveletName, mode=self.waveletMode))
+      y = fft.fft2(pywt.waverec2(array2pywt(inV), wavelet=self.waveletName, mode=self.waveletMode), norm='ortho')
 
       # Select frequencies as per sampling set
       if( self.samplingSet is None ):
@@ -250,13 +258,16 @@ class Operator4dFlow(genericOperator):
     
       # Apply Fourier Transform Only for frequencies in the sampling set
       if( self.samplingSet is None ):
-        arr = np.conj( fft.fft2( np.conj(x) ) )
+        # arr = np.conj( fft.fft2( np.conj(x) ) )
+        arr = np.conj( fft.fft2( np.conj(x), norm='ortho' ) )
       else:
         self._buffer[ self.samplingSet ] = x[ : ];
-        arr = np.conj( fft.fft2( np.conj(self._buffer) ) )
+        # arr = np.conj( fft.fft2( np.conj(self._buffer) ) )
+        arr = np.conj( fft.fft2( np.conj(self._buffer), norm='ortho' ) )
 
       # Perform wavelet transform
-      res = self._cst * pywt2array(pywt.wavedec2(arr, wavelet=self.waveletName, mode=self.waveletMode),arr.shape)
+      # res = self._cst * pywt2array(pywt.wavedec2(arr, wavelet=self.waveletName, mode=self.waveletMode),arr.shape)
+      res = pywt2array(pywt.wavedec2(arr, wavelet=self.waveletName, mode=self.waveletMode),arr.shape)
 
       # Filter wavelet coefficients as per basisSet
       if( self.basisSet is None ):

genSamples.py
@@ -6,8 +6,7 @@ import numpy.fft as fft
 import numpy.linalg as la
 from CSRecoverySuite import generateSamplingMask, crop
 import argparse
-
-home = os.getenv('HOME')
+import matplotlib.pyplot as plt
 
 def getVenc(vel):
     mx = np.amax(np.fabs(vel))
@@ -34,10 +33,10 @@ def getKspace(sample, venc, magnitudes=None, referencephase=None):
         compyk = np.zeros(vel.shape, dtype=complex)
         #2d fourier transform each slice for k space
         for k in range(sample.shape[2]): #number of 2d images in grid
-            yk[n,0,k] = fft.fft2(mag[k]*np.exp(1j*refphase[k]))
+            yk[n,0,k] = fft.fft2(mag[k]*np.exp(1j*refphase[k]),norm='ortho')
             for j in range(0,sample.shape[1]-1): #number of velocity components
                 compyk[j,k] = magnitudes[n,j+1,k]*np.exp(1j*refphase[k])*np.exp(np.pi*1j*vel[j,k]/venc)
-                yk[n,j+1,k] = fft.fft2(compyk[j,k])
+                yk[n,j+1,k] = fft.fft2(compyk[j,k],norm='ortho')
     return yk 
 
 def undersample(kspace, mask):
@@ -71,7 +70,7 @@ def add_noise(kspace, noise_percent, num_realizations, num_components=4):
     nrm = [None]*num_components
     for v in range(kspace.shape[1]):
         nrm[v] = la.norm(kspace[0,v])
-    print('Image component norms: ', nrm)
+    print('K-space image component two-norms: ', nrm)
     noise, snr = get_noise(imsz, nrm, noise_percent, num_realizations , num_components)
     for n in range(num_realizations):
         for v in range(kspace.shape[1]):
@@ -106,7 +105,6 @@ def genSamples(fromdir,numRealizations,truefile,tosavedir,uType,uVal,uSeed,noise
     if(printlevel>0):
         print('Generate and save sampling mask...')  
     
-
     undfile = tosavedir + 'undersamplpattern_p' + str(int(uVal*100)) + uType # + '_seed' + str(uSeed)       
     if useMultiPatterns:
       numPatterns = numRealizations
@@ -121,8 +119,8 @@ def genSamples(fromdir,numRealizations,truefile,tosavedir,uType,uVal,uSeed,noise
     if(printlevel>0):
         print('Add noise to image...')
     noisy,snr = add_noise(kspace, noisePercent, numRealizations)
-    fourier_file = tosavedir + 'noisy_noise' + str(int(noisePercent*100)) 
-    np.save(fourier_file + '_n' + str(numRealizations), noisy)
+    fourier_file = tosavedir + 'noisy_noise' + str(int(noisePercent*100)) + '_n' + str(numRealizations)
+    np.save(fourier_file, noisy)
     
     # Save SNR
     if(printlevel>0):

mseplt.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import warnings
 sys.path.append('../')
 from recover import linear_reconstruction, recover_vel
 from genSamples import getKspace
@@ -10,6 +11,8 @@ from matplotlib.ticker import PercentFormatter, ScalarFormatter
 from CSRecoverySuite import crop, isvalidcrop, extractFluidMask
 from CSRecoverySuite import get_umask_string, get_method_string, get_wavelet_string
 import argparse
+
+warnings.filterwarnings('error')
 
 home = os.getenv('HOME')
 hatchPatterns = ['-', '+', 'x', '\\', '*', 'o', 'O', '.']
@@ -86,10 +89,12 @@ def generateBarPlot(lgd,xlabel,ylabel,
   yVals_mag = np.zeros(len(allplt_mag))
   for loopA in range(len(allplt_mag)):
     yVals_mag[loopA] = allplt_mag[loopA].mean()
+  
   # Angle
   yVals_ang = np.zeros(len(allplt_ang))
   for loopA in range(len(allplt_ang)):
     yVals_ang[loopA] = allplt_ang[loopA].mean()
+
   # Complex
   yVals_cmx = np.zeros(len(allplt_cmx))
   for loopA in range(len(allplt_cmx)):
@@ -133,10 +138,12 @@ def generateBarPlot(lgd,xlabel,ylabel,
     yVals_mag = np.zeros(len(allplt_mag_lin))
     for loopA in range(len(allplt_mag_lin)):
       yVals_mag[loopA] = allplt_mag_lin[loopA].mean()
+
     #
     yVals_ang = np.zeros(len(allplt_ang_lin))
     for loopA in range(len(allplt_ang_lin)):
       yVals_ang[loopA] = allplt_ang_lin[loopA].mean()
+
     #
     yVals_cmx = np.zeros(len(allplt_cmx_lin))
     for loopA in range(len(allplt_cmx_lin)):
@@ -201,7 +208,7 @@ def get_files(dir, maskdir, noise, uval, utype, wavelet, method, numsamples):
   if(os.path.exists(orig_file)):
     try:
       orig = np.load(orig_file).astype(np.complex)
-    except:
+    except Warning:
       print('ERROR: Cannot read original image - ',orig)
       sys.exit(-1)
     if(not(isvalidcrop(orig))):
@@ -215,7 +222,7 @@ def get_files(dir, maskdir, noise, uval, utype, wavelet, method, numsamples):
   if(os.path.exists(fourier_file)):
     try:
       fourier = np.load(fourier_file).astype(np.complex)
-    except:
+    except Warning:
       print('ERROR: Cannot read Fourier file - ',fourier_file)
       sys.exit(-1)
   else:
@@ -226,7 +233,7 @@ def get_files(dir, maskdir, noise, uval, utype, wavelet, method, numsamples):
   if(os.path.exists(mask_file)):
     try:
       mask = np.load(mask_file).astype(bool)
-    except:
+    except Warning:
       print('ERROR: Cannot read binary mask - ',mask_file)
       sys.exit(-1)
   else:
@@ -237,7 +244,7 @@ def get_files(dir, maskdir, noise, uval, utype, wavelet, method, numsamples):
   if(os.path.exists(recovered_file)):
     try:
       recovered = np.load(recovered_file).astype(np.complex)
-    except:
+    except Warning:
       print('ERROR: Cannot read reconstructedd image - ',recovered_file)
       sys.exit(-1)
   else:
@@ -308,12 +315,14 @@ def get_mse_mag(k,csimgs,refimg,useFluidMask=False,fluidMask=None):
     imgDen = np.absolute(refimg[0,0])**2
     res = np.mean(imgNum[fluidMask])
     den = np.mean(imgDen[fluidMask])
+
     if(den > 1.0e-12):
       res = res/den
   else:
     res = np.mean((np.absolute(refimg[0,0])-np.absolute(csimgs[k,0]))**2)    
     if(np.mean(np.absolute(refimg[0,0])**2) > 1.0e-12):
       res = res/np.mean(np.absolute(refimg[0,0])**2)    
+
   return res
 
 def get_mse_ang(k,csimgs,refimg,useFluidMask=False,fluidMask=None):
@@ -323,6 +332,7 @@ def get_mse_ang(k,csimgs,refimg,useFluidMask=False,fluidMask=None):
   else:
     res = np.mean((np.angle(refimg[0,0])-np.angle(csimgs[k,0]))**2)   
     # mse_ang_cs[k]  = mse_ang_cs[k]/np.mean(np.angle(refimg)**2)
+
   return res
 
 def get_mse_cmx(k,csimgs,refimg,useFluidMask=False,fluidMask=None):
@@ -331,12 +341,15 @@ def get_mse_cmx(k,csimgs,refimg,useFluidMask=False,fluidMask=None):
     imgDen = np.absolute(refimg[0,0])**2
     res = np.mean(imgNum[fluidMask])
     den = np.mean(imgDen[fluidMask])
+
     if(den > 1.0e-12):
       res = res/den    
   else:
+
     res = np.mean((np.absolute(refimg[0,0]-csimgs[k,0]))**2)
     if(np.mean(np.absolute(refimg[0,0])**2) > 1.0e-12):
       res = res/np.mean(np.absolute(refimg[0,0])**2)
+
   return res
 
 def get_error(channel, dir, maskdir, noise, uval, utype, wavelet, method, numsamples, usecompleximgs, usetrueimg, addlinearrec, useFluidMask):
@@ -350,13 +363,30 @@ def get_error(channel, dir, maskdir, noise, uval, utype, wavelet, method, numsam
   # If use fluid mask, then compute the mask using the original image density+velocities
   fluidMask = None
   if(useFluidMask):
-    fluidMask = extractFluidMask(orig)
+    orig_forMask = np.load(dir + 'imgs_n1.npy').astype(np.complex)
+    fluidMask = extractFluidMask(orig_forMask)
+    # print('fluid mask: ',fluidMask.shape,fluidMask.sum(),np.prod(fluidMask.shape))
 
   # Get average cs and linear reconstructed image
   if(usetrueimg):
     refimg = orig
   else:
     refimg = csimgs.mean(axis=0,keepdims=True)
+
+  # DEBUG - CHECK THE DIFFERENCE IN RECONSTRUCTED IMAGE
+  print('orig ',orig.shape)
+  print('csimgs ',csimgs.shape)
+  plt.figure()
+  plt.subplot(1,2,1)
+  plt.title('TRUE')
+  plt.imshow(np.absolute(refimg[0,0]))
+  plt.colorbar()
+  plt.subplot(1,2,2)
+  plt.title('REC')
+  plt.imshow(np.absolute(csimgs[0,0]))
+  plt.colorbar()
+  plt.show()
+  # exit(-1)
 
   # print('Sizes')
   # print('csimgs:',csimgs.shape)

paper/linRecTest.py
@@ -44,7 +44,7 @@ plt.close()
 
 # Transform to k-space
 print('Transform in k-space...')
-kspace = np.fft.fft2(orimg)
+kspace = np.fft.fft2(orimg,norm='ortho')
 
 # Add noise 
 print('Add noise...')

recover.py
@@ -37,6 +37,7 @@ def get_eta(im, imNrm, noise_percent, m):
   return eta
 
 def recoverOne(kspace, imsz, eta, omega, wvlt='haar', solver_mode=CS_MODE, omp_mode=def_omp_mode, omp_iter=def_omp_iter, omp_ts=def_omp_ts):
+  
   A = OperatorWaveletToFourier(imsz, samplingSet=omega, waveletName=wvlt)
   
   wim = pywt.wavedec2(fft.ifft2(kspace, norm='ortho'), wavelet=wvlt, mode='zero')
@@ -69,8 +70,10 @@ def recoverOne(kspace, imsz, eta, omega, wvlt='haar', solver_mode=CS_MODE, omp_m
     wim =  MinimizeSumOfSquares(yim, Adeb)
     if isinstance(wim, tuple):
       wim = wim[0] # for the case where ynrm is less than eta
+
+  # Reconstruct Image from wavelet coefficients
   csim = A.getImageFromWavelet(wim)
-  
+
   return csim
   
 def recover(noisy, original, pattern, noise_percent, processnum, return_dict, wvlt='haar', solver_mode=CS_MODE, omp_mode=def_omp_mode, omp_iter=def_omp_iter, omp_ts=def_omp_ts):    
@@ -92,7 +95,7 @@ def recover(noisy, original, pattern, noise_percent, processnum, return_dict, wv
         imNrm=np.linalg.norm(im.ravel(), 2)
         eta = get_eta(im, imNrm, noise_percent, imsz[0])
         cs[n,k,j] = recoverOne(crop(noisy[n,k,j]), imsz, eta, omega, wvlt, solver_mode, omp_mode, omp_iter, omp_ts)
-        print('Recovered! Repetition: %d, Image: %d, Third: %d' % (n,k,j))
+        print('Recovered! Repetition: %d, Image: %d, Component: %d' % (n,k,j))
 
   return_dict[processnum] = cs
   return cs
@@ -118,7 +121,7 @@ def recoverAll(fourier_file, orig_file, pattern, noise_percent, c=2, wvlt='haar'
   return_dict = manager.dict()
   jobs = []
   imsz = crop(original[0,0,0]).shape
-  first = original[0,0,0]
+  first = original[0,0,0]  
   wsz = pywt2array(pywt.wavedec2(crop(first), wavelet=wvlt, mode='periodization'), imsz).shape
   for n in range(0, data.shape[0], interval):
     if pattern.shape[0] > 1:
@@ -187,7 +190,7 @@ def linear_reconstruction(fourier_file, omega=None):
       for j in range(kspace.shape[2]):
         if omega is not None:
           kspace[n,k,j][~omega] = 0
-        linrec[n,k,j] = fft.ifft2(crop(kspace[n,k,j]))
+        linrec[n,k,j] = fft.ifft2(crop(kspace[n,k,j]), norm='ortho')
   return linrec
 
 def getMethodString(mtd):
@@ -437,7 +440,7 @@ if __name__ == '__main__':
   # Perform Reconstruction
   if not os.path.exists(rec_file):
 
-    print('Loading undersampling mask...')
+    print('Loading undersampling mask...')    
     if os.path.exists(mask_file):
       umask = np.load(mask_file)
     else:

saveimgs.py
@@ -3,32 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import cv2
-sys.path.append('../')
 from recover import recover_vel, linear_reconstruction
 import argparse
-
-home = os.getenv("HOME")
-
-def rescale(img, newmax=255, truncate=True, truncateMax=1):
-  '''
-  Rescale Image from 0 to 255 and optionally truncate between 0 and truncateMax
-  '''
-  if truncate:
-    img[img < 0] = 0
-    img[img > truncateMax] = truncateMax
-
-  newimg = np.zeros(img.shape)
-  for n in range(img.shape[0]):
-    for k in range(img.shape[1]):
-      minimum = np.amin(img[n,k])
-      maximum = np.amax(img[n,k])
-      if (maximum-minimum > 1E-3):
-        newimg[n,k] = (img[n,k]-minimum)*(newmax/(maximum - minimum))
-      else:
-        newimg[n,k] = np.zeros(newimg[n,k].shape)
-
-  print('Image rescaled: new minimum: %f, new maximum: %f ' % (np.amin(newimg), np.amax(newimg)))
-  return newimg
 
 def pltimg(img, title):
   if len(img.shape)>2:
@@ -62,10 +38,7 @@ def save_rec(infilename, venc, p, samptype, noise_percent, wavetype, algtype, pr
     imgs = np.absolute(recovered)
   else:
     imgs = recover_vel(recovered, venc)
-  
-  if(relative and not(singleChannel)):
-    imgs = rescale(imgs)
-  
+    
   # Only the first reconstructed Sample
   for n in range(1): #range(imgs.shape[0]):
     for k in range(imgs.shape[1]):
@@ -84,8 +57,9 @@ def save_rec(infilename, venc, p, samptype, noise_percent, wavetype, algtype, pr
           myvmin = -1.2205644845962524
           myvmax = 1.3197449445724487
       else:
-        myvmin=None
-        myvmax=None
+        # ASSUMES INTENSITY AND VELOCITIES ARE IN [0,1]
+        myvmin=0
+        myvmax=1
 
       plt.imshow(imgs[n,k,0], cmap='gray',vmin=myvmin,vmax=myvmax)
       plt.axis('off')
@@ -95,6 +69,7 @@ def save_rec(infilename, venc, p, samptype, noise_percent, wavetype, algtype, pr
                                           '_w' + str(wavetype) + \
                                           '_a' + str(algtype) + \
                                           '_k' + str(k) + '.png', bbox_inches='tight', pad_inches=0)
+      plt.close()
 
 def save_rec_noise(recnpyFile, orig_file, venc, p, samptype, noise_percent, wavetype, algtype, prefix, outputdir, singleChannel=False, use_truth=False, ext='.png'):
   
@@ -119,36 +94,50 @@ def save_rec_noise(recnpyFile, orig_file, venc, p, samptype, noise_percent, wave
   if(not(use_truth)):
     avg = imgs.mean(axis=0)
 
+  # # DEBUG - CHECK THE DIFFERENCE IN RECONSTRUCTED IMAGE
+  # plt.figure()
+  # plt.subplot(1,2,1)
+  # plt.title('TRUE')
+  # plt.imshow(np.absolute(true[0,3,0]))
+  # plt.colorbar()
+  # plt.subplot(1,2,2)
+  # plt.title('REC')
+  # plt.imshow(np.absolute(imgs[0,3,0]))
+  # plt.colorbar()
+  # plt.show()
+  # exit(-1)
+
   # Substract images either from the truth or the average reconstruction
+  imgRes = np.zeros_like(imgs)
   for i in range(imgs.shape[0]):
     if use_truth:
       boolMask = np.logical_or((np.absolute(imgs[i,:,:,:,:]) < 1.0e-12),(np.absolute(true[0,:,:,:,:]) < 1.0e-12))
       with np.errstate(divide='ignore', invalid='ignore'):
-        imgs[i,:,:,:,:] = 2*(imgs[i,:,:,:,:] - true[0,:,:,:,:])/(np.absolute(imgs[i,:,:,:,:]) + np.absolute(true[0,:,:,:,:]))      
-      imgs[i,boolMask] = 0.0
+        imgRes[i,:,:,:,:] = 2*(imgs[i,:,:,:,:] - true[0,:,:,:,:])/(np.absolute(imgs[i,:,:,:,:]) + np.absolute(true[0,:,:,:,:]))      
+      imgRes[i,boolMask] = 0.0
     else:
       boolMask = np.logical_or((np.absolute(imgs[i,:,:,:,:]) < 1.0e-12),(np.absolute(true[0,:,:,:,:]) < 1.0e-12))
       with np.errstate(divide='ignore', invalid='ignore'):
-        imgs[i,:,:,:,:] = 2*(imgs[i,:,:,:,:] - avg[:,:,:,:])/(np.absolute(imgs[i,:,:,:,:]) + np.absolute(avg[:,:,:,:]))
-      imgs[i,boolMask] = 0.0
+        imgRes[i,:,:,:,:] = 2*(imgs[i,:,:,:,:] - avg[:,:,:,:])/(np.absolute(imgs[i,:,:,:,:]) + np.absolute(avg[:,:,:,:]))
+      imgRes[i,boolMask] = 0.0
       
   if(use_truth):
     desc = 'true'
   else:
     desc = 'avg'
 
-  print(desc + " MSE ", ((imgs)**2).mean())
-
   for n in range(1): # range(imgs.shape[0]): # Loop on the number of samples
-    for k in range(imgs.shape[1]):
-      plt.imshow(imgs[n,k,0], cmap='seismic', vmin=-2, vmax=2)
+    for k in range(imgRes.shape[1]):
+      plt.imshow(imgRes[n,k,0], cmap='seismic', vmin=-2, vmax=2)
       plt.axis('off')
+      # plt.show()
       plt.savefig(outputdir + prefix + desc + 'recerror' + '_p' + str(int(p*100)) + samptype + \
                                                        '_noise' + str(int(noise_percent*100)) + \
                                                            '_n' + str(n) + \
                                                            '_w' + str(wavetype) + \
                                                            '_a' + str(algtype) + \
                                                            '_k' + str(k) + ext, bbox_inches='tight', pad_inches=0)
+      plt.close()
       
 def save_noisy(noise_percent, dir, recdir, venc, num_samples, relative=False, ext='.png'):
     noisy = np.load(dir + 'noisy_noise' + str(int(noise_percent*100)) + '_n' + str(num_samples) + '.npy')
@@ -156,8 +145,8 @@ def save_noisy(noise_percent, dir, recdir, venc, num_samples, relative=False, ex
     avg = noisy.mean(axis=0)
     orig_file = dir+'imgs_n1' +  '.npy'
     imgs = recover_vel(noisy, venc)
-    if relative:
-        imgs = rescale(imgs)
+    # if relative:
+    #     imgs = rescale(imgs)
     directory = recdir + 'noise' + str(int(noise_percent*100))
     if not os.path.exists(directory):
         os.makedirs(directory)
@@ -167,8 +156,8 @@ def save_noisy(noise_percent, dir, recdir, venc, num_samples, relative=False, ex
 
 def save_true(trueFileName, outputDir, relative=False):
   true = np.load(trueFileName)
-  if relative:
-    true = rescale(true, truncate=False)
+  # if relative:
+  #  true = rescale(true, truncate=False)
   for k in range(true.shape[1]):
     if(False):
       print('Min True: ',np.min(true[0,k,0]))