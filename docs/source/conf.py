596b91305b9e56d7708ee6a5e5f6febb4e181707
des <dschiavazzi@nd.edu>
2022-01-03 18:37:40 -0500

    changed path in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,9 +5,9 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Path setup ----------------------------------------
-import os
-import sys
-sys.path.insert(0, os.path.abspath('..'))
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
