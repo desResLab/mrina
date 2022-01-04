7d57e106d8c70b4c6669d3c6f823165c55612f79
des <dschiavazzi@nd.edu>
2022-01-04 12:40:37 -0500

    moved extension out of setup

--- setup.py
+++ setup.py
@@ -14,6 +14,8 @@ HERE = path.abspath(path.dirname(__file__))
 with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+extensions = [Extension("mrina.solver_omp", ["mrina/solver_omp.pyx"])]
+
 # This call to setup() does all the work
 setup(
     name="mrina",
@@ -39,7 +41,6 @@ setup(
     packages=["mrina"],
     include_package_data=True,
     install_requires=["numpy"],
-    extensions = [Extension("mrina.solver_omp", ["mrina/solver_omp.pyx"])],
     ext_modules = cythonize(extensions,
                             annotate=True,
                             compiler_directives={'language_level':3}),
