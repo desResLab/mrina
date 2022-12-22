29376da189a34575ddd387db1fc362a7f87a15ae
des <dschiavazzi@nd.edu>
2022-12-22 15:14:29 +0100

    difference setup

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ HERE = path.abspath(path.dirname(__file__))
 with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-extensions = [Extension("mrina.solver_omp", ["mrina/solver_omp.pyx"])]
+extensions = [Extension("mrina.solver_omp", ["solver_omp.pyx"])]
 
 # This call to setup() does all the work
 setup(
@@ -44,7 +44,7 @@ setup(
     ext_modules = cythonize(extensions,
                             annotate=True,
                             compiler_directives={'language_level':3},
-                            include_path=[HERE]
+                            include_path=[path.join(HERE, 'mrina/')]
                             ),
     include_dirs=[np.get_include(),HERE]
 )
