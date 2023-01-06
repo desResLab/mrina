5e580ee9775e59d284676746a2d6bc4ef3feacfe
des <dschiavazzi@nd.edu>
2023-01-06 16:07:53 +0100

    version 0.3.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ extensions = [Extension("mrina.solver_omp", ["mrina/solver_omp.pyx"])]
 # This call to setup() does all the work
 setup(
     name="mrina",
-    version="0.2.8",
+    version="0.3.0",
     description="Library for MRI noise analysis",
     long_description=long_description,
     long_description_content_type="text/markdown",
